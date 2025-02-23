edd3653823dbaf166cefe45dcc5b433760752ab0
root <root@instance-20250220-021400.asia-south1-c.c.t-system-451214-n4.internal>
2025-02-23 14:59:28 +0000

    2nd commit

--- src/server.ts
+++ src/server.ts
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
 }) 
 
 app.listen(process.env.PORT, () => {
-   console.log(`Example app listening on port ${process.env.PORT}`)
+   console.log(`Example App listening on port ${process.env.PORT}`)
 })
