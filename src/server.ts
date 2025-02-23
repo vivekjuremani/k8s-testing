451ce79f122582349977b82b55c7bd276fcf41f1
vivek <vivekjuremani@gmail.com>
2025-02-23 20:10:16 +0530

    intial commmit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config(
+ { path : "./config/.env" }  
+)
+app.get('/', (req, res) => {
+ 
+ const envVaraiables= {
+   NODE_ENV: process.env.NODE_ENV,
+   PORT: process.env.PORT,
+   HOST : process.env.HOST
+ }
+
+ res.send(JSON.stringify(envVaraiables, null, 2))   
+}) 
+
+app.listen(process.env.PORT, () => {
+   console.log(`Example app listening on port ${process.env.PORT}`)
+})
